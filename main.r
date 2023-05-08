249747c43762e99b9ab561f0dcc3876cb76ca92a
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-05-08 19:28:39 -0300

    Decomposicao STL

--- main.r
+++ main.r
@@ -2,5 +2,8 @@ pacman::p_load("Mcomp", "dplyr")
 
 ids <- c(1277, 1903)
 
-M3[[ids[1]]] %>% plot()
-M3[[ids[2]]] %>% plot()
+current_data <- M3[[ids[1]]]
+current_data %>% plot()
+
+xy_train <- current_data$x
+stl(xy_train, s.window = "periodic") %>% plot()
