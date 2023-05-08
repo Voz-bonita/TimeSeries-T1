dd6535781bffbcca6b8faa5ad9dfeb26507b46d4
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-05-08 19:37:42 -0300

    Plot basico por ggplot

--- main.r
+++ main.r
@@ -1,9 +1,26 @@
-pacman::p_load("Mcomp", "dplyr")
+pacman::p_load("Mcomp", "dplyr", "ggplot2")
 
 ids <- c(1277, 1903)
 
 current_data <- M3[[ids[1]]]
 current_data %>% plot()
+current_data$n
+xy <- c(as.vector(current_data$x), as.vector(current_data$xx))
+xy_time <- c(as.vector(time(current_data$x)), as.vector(time(current_data$xx)))
+
+plot_df <- data.frame(
+    "Ano" = xy_time,
+    "Y" = xy,
+    "Tipo" = c(
+        rep("Treino", current_data$n),
+        rep("Teste", length(xy) - current_data$n)
+    )
+)
+
+ggplot(data = plot_df) +
+    geom_line(aes(x = `Ano`, y = `Y`, color = `Tipo`), size = 1) +
+    scale_color_manual(values = c("red", "black")) +
+    theme_bw()
 
 xy_train <- current_data$x
 stl(xy_train, s.window = "periodic") %>% plot()
