182526194fa1488e4976951de83325ab42971a31
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-05-08 19:55:00 -0300

    Linhas sazonais para analise

--- main.r
+++ main.r
@@ -1,8 +1,9 @@
 pacman::p_load("Mcomp", "dplyr", "ggplot2")
 
+PERIOD_TO_VALUE <- c("QUARTERLY" = 4, "MONTHLY" = 1)
 ids <- c(1277, 1903)
 
-current_data <- M3[[ids[2]]]
+current_data <- M3[[ids[1]]]
 current_data %>% plot()
 current_data$n
 xy <- c(as.vector(current_data$x), as.vector(current_data$xx))
@@ -21,6 +22,18 @@ ggplot(data = plot_df) +
     geom_line(aes(x = `Ano`, y = `Y`, color = `Tipo`), size = 1) +
     scale_color_manual(values = c("red", "black")) +
     theme_bw()
+
+
+period_value <- PERIOD_TO_VALUE[current_data$period]
+seasonal_obs_date <- plot_df[["Ano"]][
+    as.numeric(rownames(plot_df)) %% period_value == 0
+]
+
+ggplot(data = plot_df) +
+    geom_line(aes(x = `Ano`, y = `Y`, color = `Tipo`), size = 1) +
+    geom_vline(xintercept = seasonal_obs_date, linetype = "dotted") +
+    scale_color_manual(values = c("red", "black")) +
+    theme_void()
 
 xy_train <- current_data$x
 stl(xy_train, s.window = "periodic") %>% plot()
