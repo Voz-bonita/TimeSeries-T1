8b9f208e830c83313047b03cd87fbcd8c26af9a3
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-05-09 14:09:55 -0300

    Graficos salvos para analise

--- main.r
+++ main.r
@@ -1,10 +1,10 @@
-pacman::p_load("Mcomp", "dplyr")
+pacman::p_load("Mcomp", "dplyr", "glue")
 source("./auxiliar.r", encoding = "UTF-8")
 
 PERIOD_TO_VALUE <- c("QUARTERLY" = 4, "MONTHLY" = 1)
 ids <- c(1277, 1903)
-
-current_data <- M3[[ids[1]]]
+ID <- ids[1]
+current_data <- M3[[ID]]
 
 xy <- c(as.vector(current_data$x), as.vector(current_data$xx))
 xy_time <- c(as.vector(time(current_data$x)), as.vector(time(current_data$xx)))
@@ -21,8 +21,10 @@ plot_df <- data.frame(
 period_value <- PERIOD_TO_VALUE[current_data$period]
 
 # Serie seguida de facilitador da analise de sazonalidade
-ggplot_series(plot_df)
-ggplot_series(plot_df, seasonal_lines = TRUE, period_value)
+ggplot_series(plot_df) %>%
+    ggsave(glue("assets/Serie{ID}_0.png"), .)
+ggplot_series(plot_df, seasonal_lines = TRUE, period_value) %>%
+    ggsave(glue("assets/Serie{ID}_s.png"), .)
 
 xy_train <- current_data$x
 stl(xy_train, s.window = "periodic") %>% plot()
