bc7a3240da3d42ba7fca615767fad7799614b30b
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-05-14 17:00:38 -0300

    Plot da série organiza os dados em data.frame

--- auxiliar.r
+++ auxiliar.r
@@ -1,6 +1,21 @@
 pacman::p_load("ggplot2")
 
-ggplot_series <- function(plot_df, seasonal_lines = FALSE, period_value = 0) {
+ggplot_series <- function(current_data, seasonal_lines = FALSE, period_value = 0) {
+    xy <- c(as.vector(current_data$x), as.vector(current_data$xx))
+    xy_time <- c(
+        as.vector(time(current_data$x)),
+        as.vector(time(current_data$xx))
+    )
+
+    plot_df <- data.frame(
+        "Ano" = xy_time,
+        "Y" = xy,
+        "Tipo" = c(
+            rep("Treino", current_data$n),
+            rep("Teste", current_data$h)
+        )
+    )
+
     plot <- ggplot(data = plot_df) +
         geom_line(aes(x = `Ano`, y = `Y`, color = `Tipo`), linewidth = 1) +
         scale_color_manual(values = c("red", "black")) +
--- main.r
+++ main.r
@@ -6,24 +6,12 @@ ids <- c(1277, 1903)
 ID <- ids[1]
 current_data <- M3[[ID]]
 
-xy <- c(as.vector(current_data$x), as.vector(current_data$xx))
-xy_time <- c(as.vector(time(current_data$x)), as.vector(time(current_data$xx)))
-
-plot_df <- data.frame(
-    "Ano" = xy_time,
-    "Y" = xy,
-    "Tipo" = c(
-        rep("Treino", current_data$n),
-        rep("Teste", current_data$h)
-    )
-)
-
 period_value <- PERIOD_TO_VALUE[current_data$period]
 
 # Serie seguida de facilitador da analise de sazonalidade
-ggplot_series(plot_df) %>%
+ggplot_series(current_data) %>%
     ggsave(glue("assets/Serie{ID}_0.png"), .)
-ggplot_series(plot_df, seasonal_lines = TRUE, period_value) %>%
+ggplot_series(current_data, seasonal_lines = TRUE, period_value) %>%
     ggsave(glue("assets/Serie{ID}_s.png"), .)
 
 xy_train <- current_data$x
