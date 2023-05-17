01ac9dfe4765565f96b4fe0ac52bcf951fdadcf6
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-05-16 22:08:59 -0300

    Gráfico PACF dos resíduos

--- residuos.r
+++ residuos.r
@@ -19,11 +19,12 @@ residuo <- modelo$residuals
 # Visualização
 
 png("assets/residuos1277.png")
-par(mfrow = c(1, 3))
+par(mfrow = c(2, 2))
 plot(residuo)
 qqnorm(residuo)
 qqline(residuo)
 acf(residuo, lag.max = length(modelo))
+pacf(residuo, lag.max = length(modelo))
 
 dev.off()
 
